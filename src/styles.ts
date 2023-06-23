a139c550949bc61e4cf277931d1dbf5cebe32b71
mateus <m4teusmatos@gmail.com>
2023-06-23 17:28:37 -0300

    style: create new style for page

--- src/styles.ts
+++ src/styles.ts
@@ -1,87 +1,58 @@
-import styled from "styled-components";
-/* padding: ${(props) => props.p}; */
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
-  max-width: 450px;
-  padding: 1.8rem 2.4rem;
-  border-radius: 18px;
-  box-shadow: 0 18px 200px -60px #313030;
+  max-width: 100%;
+  height: 100%;
+  padding: 24px;
+  gap: 30px;
 `;
 
-export const WrapperImg = styled.div`
-  margin: 0 auto;
-`;
+export const WrapperAvatar = styled.div`
+  width: 300px;
+  height: 300px;
 
-export const Img = styled.img`
-  width: 200px;
-  height: 200px;
-  border-radius: 100px;
-`;
-
-export const WrapperDesc = styled.div``;
-
-export const Title = styled.h1`
-  font-family: "Nunito", sans-serif;
-  padding: 0.4rem 0;
-  text-align: center;
-`;
-
-export const SubTitle = styled.p`
-  font-size: 1em;
-  color: #515050;
-  font-family: "Nunito", sans-serif;
-  text-align: center;
-  padding-bottom: 0.6rem;
-`;
-
-export const Text = styled.p`
-  font-size: 1em;
-  font-family: "Nunito", sans-serif;
-  text-align: center;
-  padding-bottom: 1.2rem;
-`;
-
-export const About = styled.p`
-  text-align: center;
-  font-family: "Nunito", sans-serif;
-`;
-
-export const WrapperLocation = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  padding-bottom: 1.2rem;
-`;
-
-export const WrapperSocial = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 60px;
-  padding-top: 0.8rem;
-  margin: 0 auto;
-`;
-
-export const BgIcon = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.6rem;
-  border-radius: 14px;
-  cursor: pointer;
-  transition all .2s;
-  &:hover {
-    transform: scale(1.2) translateY(-5px);
+  @media (max-width: 450px) {
+    width: 200px;
+    height: 200px;
   }
 `;
 
-export const WrapperButtons = styled.div`
+export const Avatar = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const Description = styled.p`
+  max-width: 800px;
+  font-family: "Poppins", sans-serif;
+  font-size: 18px;
+  color: rgb(245, 245, 239);
+  text-align: center;
+
+  @media (max-width: 450px) {
+    font-size: 15px;
+  }
+`;
+
+export const WrapperSocialIcons = styled.div`
   display: flex;
-  align-items: center;
-  gap: 60px;
-  margin: 0 auto;
-  padding: 1.2rem 0 1.2rem 0;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 14px;
+`;
+
+export const IconLink = styled.a`
+  display: inline-block;
+  transition: transform 0.2s ease;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
 `;
