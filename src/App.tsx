489cc1e9343429cd03e11696bf7dfb34b61ffc27
mateus <m4teusmatos@gmail.com>
2023-06-27 20:19:32 -0300

    import components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,16 @@
-import * as Style from './styles';
-import { FaInstagram } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaLinkedinIn } from 'react-icons/fa';
-import { useTranslation, initReactI18next } from "react-i18next";
-import i18n from "i18next";
-import Me from './assets/me.jpeg';
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-          "Description": "Hello, I'm Mateus! Computer science student and mobile developer with React Native. 🤓",
-        }
-      },
-      pt: {
-        translation: {
-          "Description": "Olá, eu sou Mateus! Estudante de ciência da computação e desenvolvedor mobile com React Native. 🤓",
-        }
-      }
-    },
-    lng: "en",
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false
-    }
-  });
+import { useState } from "react";
+import { About, Footer, Header } from "./components"
 
 function App() {
 
-  const { t } = useTranslation();
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <Style.Container>
-      <Style.WrapperAvatar>
-        <Style.Avatar src={Me} />
-      </Style.WrapperAvatar>
-      <Style.Description>
-        {t('Description')}
-      </Style.Description>
-      <Style.WrapperSocialIcons>
-        <Style.IconLink href='https://www.linkedin.com/in/mateusmatosleonardo'>
-          <FaLinkedinIn size={40} color='#fafafa' />
-        </Style.IconLink>
-        <Style.IconLink href='https://github.com/mateusmatosleonardo'>
-          <FaGithub size={40} color='#fafafa' />
-        </Style.IconLink>
-        <Style.IconLink href='https://instagram.com/m4teus.dev?igshid=MzNlNGNkZWQ4Mg=='>
-          <FaInstagram size={40} color='#fafafa' />
-        </Style.IconLink>
-      </Style.WrapperSocialIcons>
-    </Style.Container>
+    <div className={`w-full ${darkMode ? 'bg-zinc-800' : ''} bg-white h-screen`}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <About darkMode={darkMode} />
+      <Footer darkMode={darkMode} />
+    </div>
   )
 }
 
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import About from "./About";
+import Footer from "./Footer";
+
+export { Header, About, Footer };
