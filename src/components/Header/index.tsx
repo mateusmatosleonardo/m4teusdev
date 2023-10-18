5b8cc05aec81e1793266b5de50ce6d6ee8c64395
mateus <m4teusmatos@gmail.com>
2023-10-18 19:04:14 -0300

    refactor: starting with header refactoring

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,15 +9,23 @@ const Header = ({ darkMode, setDarkMode }: any) => {
 
   return (
     <header>
-      <div className="container px-4 py-6 mx-auto lg:max-w-4xl flex items-center justify-between ">
+      <div className="container px-4 py-6 mx-auto flex items-center justify-between ">
         <h1 className={`font-medium tracking-wider transition-colors text-gray-900 ${darkMode ? 'text-white' : ''} hover:text-blue-500 uppercase cursor-pointer`}>
           Mateus Dev
         </h1>
-        <button onClick={toggleDarkMode}>
-          {darkMode ?
-            <FaSun size={22} className={`${darkMode ? 'text-white' : ''} text-gray-800`} /> :
-            <FaMoon size={22} className="text-gray-800" />}
-        </button>
+        <div className='flex gap-x-8'>
+          <div className={`flex gap-x-8 ${darkMode ? 'text-white' : ''}`}>
+            <a href='/'>Início</a>
+            <a href='/'>Sobre</a>
+            <a href='/'>Experiência</a>
+            <a href='/'>Contato</a>
+          </div>
+          <button onClick={toggleDarkMode}>
+            {darkMode ?
+              <FaSun size={22} className={`${darkMode ? 'text-white' : ''} text-gray-800`} /> :
+              <FaMoon size={22} className="text-gray-800" />}
+          </button>
+        </div>
       </div>
     </header>
   )
