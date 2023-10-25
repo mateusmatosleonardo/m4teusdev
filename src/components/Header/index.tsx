b343e02bfcf7eeda57c6cd812f371117726f3df2
mateus <m4teusmatos@gmail.com>
2023-10-24 21:16:02 -0300

    feat: updating the hearer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,59 @@
-import { FaSun } from 'react-icons/fa';
-import { FaMoon } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
-const Header = ({ darkMode, setDarkMode }: any) => {
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+const Header = () => {
 
   return (
-    <header>
-      <div className="container px-4 py-6 mx-auto flex items-center justify-between ">
-        <h1 className={`font-medium tracking-wider transition-colors text-gray-900 ${darkMode ? 'text-white' : ''} hover:text-blue-500 uppercase cursor-pointer`}>
-          Mateus Dev
-        </h1>
-        <div className='flex gap-x-8'>
-          <div className={`flex gap-x-8 ${darkMode ? 'text-white' : ''}`}>
-            <a href='/'>Início</a>
-            <a href='/'>Sobre</a>
-            <a href='/'>Experiência</a>
-            <a href='/'>Contato</a>
-          </div>
-          <button onClick={toggleDarkMode}>
-            {darkMode ?
-              <FaSun size={22} className={`${darkMode ? 'text-white' : ''} text-gray-800`} /> :
-              <FaMoon size={22} className="text-gray-800" />}
-          </button>
+    <header className='w-full fixed z-10 backdrop-blur-lg'>
+      <div className="px-6 py-6 mx-auto">
+        <div className='flex gap-x-4'>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: -75 },
+              visible: { opacity: 1, y: 0 }
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.4, delay: 0.35 }}>
+            <a
+              href="https://www.linkedin.com/in/mateusmatosleonardo"
+              target="_blank" rel="noreferrer">
+              <FaLinkedinIn size={22} className={'hover:text-blue-500 text-gray-400'} />
+            </a>
+          </motion.div>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: -75 },
+              visible: { opacity: 1, y: 0 }
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.4, delay: 0.45 }}>
+            <a
+              href="https://twitter.com/m4teusmatos"
+              target="_blank" rel="noreferrer">
+              <FaTwitter size={22} className={'hover:text-blue-500 text-gray-400'} />
+            </a>
+          </motion.div>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: -75 },
+              visible: { opacity: 1, y: 0 }
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.4, delay: 0.55 }}>
+            <a
+              href="https://github.com/mateusmatosleonardo"
+              target="_blank" rel="noreferrer">
+              <FaGithub size={22} className={'hover:text-blue-500 text-gray-400'} />
+            </a>
+          </motion.div>
+
         </div>
       </div>
     </header>
   )
 }
 
-export default Header;
+export default Header
