2bf16c07a58284432adbcb95e2bb65f09cf5b61c
mateus <m4teusmatos@gmail.com>
2023-10-26 23:51:49 -0300

    feat: create content for experience

---
+++ src/components/Experience/content.ts
@@ -0,0 +1,11 @@
+import { type IExperience } from "./types";
+
+export const list: IExperience[] = [
+  {
+    company: "Arkos",
+    office: "Desenvolvedor Front-end",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolorum excepturi velit porro minima. Doloremque nisi adipisci illo corrupti officiis suscipit hic distinctio commodi eum error. Dolores rerum velit autem.",
+    technologiesUsed: ["oi", "sads"],
+  },
+];
